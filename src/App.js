df0d4b45f7fd9065e99c42eed825738881820e12
Kevin Ford <kevin@fastmodeltechnologies.com>
2018-07-25 14:50:51 -0500

    Broken app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import _ from 'underscore';
 
 class App extends Component {
+
+  state = {
+    posts: [],
+    usersById: {},
+  };
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/pots')
+      .then(posts => {
+
+        this.setState({ posts })
+      }));
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(users => {
+        this.setState({ usersById: user })
+      }));
+  };
+
+
   render() {
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <div className="App-header-content">
+            <h1 className="App-title">Debugging App</h1>
+          </div>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <section className="posts">
+          { this.state.posts.map(post => {
+            return (
+              <article style={{ margin: '10px 0', padding: 10, border: '1px solid #ccc'}}>
+                <p style={{ textDecoration: 'underline', fontWeight: 'bold' }}>{ post.title }</p>
+                <p>{ post.userId }</p>
+                <p>{ post.body }</p>
+              </article>
+            )
+          })}
+        </section>
       </div>
     );
   }
